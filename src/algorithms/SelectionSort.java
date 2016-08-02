0ae9605918fcf390d0fe1e092bae242e85691a92
ipritish <pritishjain2009@gmail.com>
2016-08-02 21:32:03 +0530

    selection sort

--- src/algorithms/SelectionSort.java
+++ src/algorithms/SelectionSort.java
@@ -1,9 +1,40 @@
 package algorithms;
 
 public class SelectionSort {
+	
+	public static void selectionSort(int[] a){
+		for (int i=0 ;i< a.length;i++){
+			int min_value = a[i];
+			for (int j=i;j<a.length;j++){
+				if (min_value >= a[j]){
+					min_value = a[j];
+					int temp = a[i];
+					a[i] = a[j];
+					a[j] = temp;
+				}
+			}
+
+		}
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int[] a = {2,4,5,4,4,8,7,6,2,8,4,2,1};
+		
+		int[] b = {4,6,2,8,5,0,1,5,7,8};
+		
+		selectionSort(a);
+		selectionSort(b);
+		
+		for (int i=0 ; i< a.length; i++){
+			System.out.println(a[i]);
+		}
+		
+		System.out.println("For B");
+		
+		for (int i=0 ; i< b.length; i++){
+			System.out.println(b[i]);
+		}
 
 	}
 
