dfebad40996a936a00da46adb4b81540fea1d1c4
ipritish <pritishjain2009@gmail.com>
2016-08-08 14:52:38 +0530

    data structure

---
+++ src/algorithms/datastructure/Node.java
@@ -0,0 +1,27 @@
+package algorithms.datastructure;
+
+public class Node {
+	
+	private int id;
+	
+	private Node next;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+	
+	
+
+}
---
+++ src/algorithms/datastructure/NodeTester.java
@@ -0,0 +1,10 @@
+package algorithms.datastructure;
+
+public class NodeTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
