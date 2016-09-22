6e68b5eb0b2bc20aeedef03da08ee28bac7981b2
ipritish <pritishjain2009@gmail.com>
2016-09-22 12:25:10 +0530

    refactor

--- src/algorithms/sort/QuickSort.java
+++ src/algorithms/sort/QuickSort.java
@@ -6,18 +6,20 @@ class QuickSort {
 	
 	private int input[]; 
 	private int length; 
+	
 	public void sort(int[] numbers){
 		
 		if (numbers == null || numbers.length == 0) 
-		{ 
+		{
 			return; 
-		} 
-		this.input = numbers; 
-		length = numbers.length; 
-		quickSort(0, length - 1); 
+		}
+		this.input = numbers;
+		length = numbers.length;
+		quickSort(0, length - 1);
 	} 
 	    
-	private void quickSort(int low, int high){ 
+	private void quickSort(int low, int high){
+		
 		int i = low;
 		int j = high; 
 		
@@ -25,42 +27,42 @@ class QuickSort {
 		int pivot = input[low + (high - low) / 2];
 		
 		while(i<=j){
+			
 			while (input[i] < pivot){
 				i++; 
-			} 
+			}
 			while (input[j] > pivot){
 				j--; 
 			} 
-			if (i <= j) { 
+			if (i <= j){ 
 				swap(i, j); 
-				i++; 
+				i++;
 				j--; 
-			} 
+			}
 		}
-			
+		
 		if (low < j){ 
 			quickSort(low, j); 
-		} 
+		}
 		if (i < high){ 
 			quickSort(i, high);
 		}
-	} 
+	}
 	
 	private void swap(int i, int j){ 
-		int temp = input[i]; 
+		int temp = input[i];
 		input[i] = input[j]; 
-		input[j] = temp; 
+		input[j] = temp;
 	}
 	
 	public static void main(String args[]){
+		
 		QuickSort sorter = new QuickSort();
 		int[] numbers = {6, 5, 3, 1, 8, 7, 2, 4};
 		sorter.sort(numbers);
 		
 		System.out.println(Arrays.toString(numbers));
 	}
-		
-		
-	
+
 }
 
